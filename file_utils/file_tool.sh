e53f3a63f15a9eb24d1a04ed1422f2c55a1824d4
Shirman_Work <shiming@supos.com>
2020-09-18 15:18:24 +0800

    add method:ip_change

--- file_utils/file_tool.sh
+++ file_utils/file_tool.sh
@@ -21,3 +21,15 @@ getProp() {
   prop=$(sed -n "/$1/p" "$2" | sed "s/$1=//g")
   echo "$prop"
 }
+
+# 改变文件中的ip值，ip_change 192.168.200.200
+ip_change() {
+	files=$(ls)
+	for file in ${files}
+	do
+		if [[ ${file} =~ (.*)conf ]]; then
+			echo "change ${file} ip to $1"
+			sed -i "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$1/g" "${file}"
+		fi
+	done
+}
